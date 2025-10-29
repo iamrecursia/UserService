bc857b7047b8c2d124b85d8f61466404cd27b8ee
kirillKozitskiy <kkozickij35@gmail.com>
2025-10-30 02:52:34 +0300

    feat(cache): configure RedisCacheManager with JSON serialization
    - Implemented RedisConfig with GenericJackson2JsonRedisSerializer
    - Set cache TTL to 10 minutes
    - Disabled caching of null values
    - Ensures type-safe serialization for multiple DTOs (UserResponseDto, CardResponseDto, etc.)

---
+++ src/main/java/com/kozitskiy/userservice/config/RedisConfig.java
@@ -0,0 +1,32 @@
+package com.kozitskiy.userservice.config;
+
+import com.kozitskiy.userservice.dto.response.UserResponseDto;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
+import java.time.Duration;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory){
+        RedisCacheConfiguration redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(10))
+                .disableCachingNullValues()
+                .serializeValuesWith(RedisSerializationContext.SerializationPair
+                        .fromSerializer(new GenericJackson2JsonRedisSerializer()));
+
+
+        return RedisCacheManager
+                .builder(connectionFactory)
+                .cacheDefaults(redisCacheConfiguration)
+                .build();
+    }
+}
